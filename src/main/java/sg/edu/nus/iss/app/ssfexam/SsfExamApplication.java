cc5d5600b51fe6d12395fe4ac58c7c8b3348036f
diegoalpin <diego_alpin@live.com>
2023-01-27 09:40:15 +0800

    first commit. Spring Boot managed to run at 8080

---
+++ src/main/java/sg/edu/nus/iss/app/ssfexam/SsfExamApplication.java
@@ -0,0 +1,18 @@
+package sg.edu.nus.iss.app.ssfexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SsfExamApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SsfExamApplication.class);
+
+	public static void main(String[] args) {
+		logger.debug("===================================APPLICATION IS ABOUT TO RUN================================");
+		SpringApplication.run(SsfExamApplication.class, args);
+		logger.debug("===================================APPLICATION FINISHED RUNNING================================");
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/app/ssfexam/SsfExamApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app.ssfexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
